c9423c4a77c70013819e16b8061bbed39cbb7195
Kristoffer Roupé <kristoffer.roupe@funnel.io>
2024-01-19 21:12:59 +0100

    Shader

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use macroquad::prelude::*;
 use macroquad::rand::ChooseRandom;
 use std::fs;
 
+const FRAGMENT_SHADER: &str = include_str!("starfield-shader.glsl");
+
+const VERTEX_SHADER: &str = "#version 100
+attribute vec3 position;
+attribute vec2 texcoord;
+attribute vec4 color0;
+varying float iTime;
+
+uniform mat4 Model;
+uniform mat4 Projection;
+uniform vec4 _Time;
+
+void main() {
+    gl_Position = Projection * Model * vec4(position, 1);
+    iTime = _Time.x;
+}
+";
+
 enum GameState {
     MainMenu,
     Playing,
@@ -60,8 +78,42 @@ async fn main() {
         collided: false,
     };
 
+    let mut direction_modifier: f32 = 0.0;
+    let render_target = render_target(320, 150);
+    render_target.texture.set_filter(FilterMode::Nearest);
+    let material = load_material(
+        ShaderSource::Glsl {
+            vertex: VERTEX_SHADER,
+            fragment: FRAGMENT_SHADER,
+        },
+        MaterialParams {
+            uniforms: vec![
+                ("iResolution".to_owned(), UniformType::Float2),
+                ("direction_modifier".to_owned(), UniformType::Float1),
+            ],
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+
     loop {
-        clear_background(DARKGRAY);
+        clear_background(BLACK);
+
+        material.set_uniform("iResolution", (screen_width(), screen_height()));
+        material.set_uniform("direction_modifier", direction_modifier);
+        gl_use_material(&material);
+        draw_texture_ex(
+            &render_target.texture,
+            0.,
+            0.,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(screen_width(), screen_height())),
+                ..Default::default()
+            },
+        );
+        gl_use_default_material();
 
 
         match game_state {
@@ -109,6 +161,7 @@ async fn main() {
 
                 if is_key_down(KeyCode::Right) {
                     circle.x += movement;
+                    direction_modifier += 0.05 * delta_time;
                 }
                 if is_key_down(KeyCode::Left) {
                     circle.x -= movement;
@@ -118,6 +171,7 @@ async fn main() {
                 }
                 if is_key_down(KeyCode::Up) {
                     circle.y -= movement;
+                    direction_modifier -= 0.05 * delta_time;
                 }
 
                 if is_key_pressed(KeyCode::Space) {
